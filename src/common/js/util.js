fbdaf2f7fbb522073d1c5d5af543d416296bb2fa
jianchenyi <897846097@qq.com>
2017-06-10 17:09:06 +0800

    first commit

---
+++ src/common/js/util.js
@@ -0,0 +1,23 @@
+/**
+ * Created by v_mzhzhang on 2017/5/24.
+ */
+/**
+ * 解析url参数
+ * @example ?a=123&b=456
+ * @return Object {a:123,b:456}
+ */
+export function urlParse() {
+  let url = window.location.search;
+  let obj = {};
+  let reg = /[?&][^?&]+=[^?&]+/g;
+  let arr = url.match(reg);
+  if (arr) {
+    arr.forEach((item) => {
+      let tempArr = item.substring(1).split('=');
+      let key = decodeURIComponent(tempArr[0]);
+      let val = decodeURIComponent(tempArr[1]);
+      obj[key] = val;
+    });
+  }
+  return obj;
+};
